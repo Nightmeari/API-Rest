88e3897017accf948634f80fc674681759e34ba2
Tamara Simões <94468072+v2vbyte@users.noreply.github.com>
2025-09-16 12:23:11 -0300

    Update questao2.js

--- questao2.js
+++ questao2.js
@@ -1,1 +1,10 @@
-// Resolva a questão 2 aqui
+const express = require("express");
+const filmes = require("./questao2_data.json");
+const server = express();
+server.use(express.json());
+
+// Resolva a questão 2 a partir desta linha
+
+
+// Caso queira, poderá alterar a porta
+server.listen(5001);
